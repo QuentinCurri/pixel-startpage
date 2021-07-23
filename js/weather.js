487c2602a0bf92ec668907cb9d7758082bba11d0
QuentinCurri <quentin.lee.curry@gmail.com>
2021-07-23 10:46:40 -0700

    Fixed temperature units

--- js/weather.js
+++ js/weather.js
@@ -5,6 +5,12 @@ const token = YOUR_API_TOKEN; // Your API Token from https://openweathermap.org/
 const lat = YOUR_LAT; // latitude coordinates
 const long = YOUR_LONG; // longitude coordinates
 const units = 'imperial'; // put your units here (imperial = fahrenheit, metric = celsius, standard = kelvin)
+const unitSymbols = {
+    imperial: "F",
+    metric : "C",
+    standard: "K",
+}; // Unit symbol based on what temperature units you selected.
+
 let getWeather = () => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=${units}&appid=${token}`)
     .then(response => {
@@ -13,7 +19,7 @@ let getWeather = () => {
     .then(data => {
         console.log(data);
         let tempElement = Math.round(data.main.temp);
-        temp.innerHTML = tempElement + '°F';
+        temp.innerHTML = tempElement + '°' + unitSymbols.unitName; //use unitSymbols.unitName (imperial, metric, or standard) to have the correct labels.
     })
 }
 
